bc9b92a6bab9a28ca4528d0dfbe65cfcc9f43933
caaaan <canborcbakan@gmail.com>
2024-06-20 15:15:02 +0200

    added a test file for ResponseChecker.java, will improve upon that

---
+++ src/test/java/spotify/utils/ResponseCheckerTest.java
@@ -0,0 +1,20 @@
+package spotify.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+import com.google.gson.Gson;
+import okhttp3.ResponseBody;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import retrofit2.Response;
+import spotify.api.enums.HttpStatusCode;
+import spotify.exceptions.SpotifyActionFailedException;
+import spotify.models.errors.SpotifyError;
+
+class ResponseCheckerTest {
+    @Test
+    void checkErrorBodyTest(){
+
+    }
+
+}
